bbb2461bbb64d09e1470a61258b2bc225b900c24
alexm <alex.m.work@yandex.ru>
2025-03-24 12:18:14 +0700

    added attached objects

--- src/scene/SceneClient.js
+++ src/scene/SceneClient.js
@@ -56,7 +56,6 @@ ROS3D.SceneClient.prototype.subscribe = function(){
 ROS3D.SceneClient.prototype.processMessage = function(message){
 
   message.robot_state.attached_collision_objects.forEach (element => {
-    console.log(element);
     if (element.object.operation === 0) {  // "ADD" or "MODIFY"
 
     var newMesh = new ROS3D.SceneMesh({
